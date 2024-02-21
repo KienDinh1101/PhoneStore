d4e37d7fff820dcf3177de8103f34a94f9b735f9
dinh kien <kiendt.gt@gmail.com>
2024-02-21 16:19:38 +0700

    first commit

---
+++ resources/lang/en/footer.php
@@ -0,0 +1,14 @@
+<?php
+
+  return [
+    'AboutUs'          =>      'About Us',
+    'address'          =>      'No.3 Cau Giay, Ngoc Khanh, Dong Da, Hanoi.',
+    'FollowUs'         =>      'Follow Us',
+    'visa'             =>      'Visa method',
+    'mastercard'       =>      'MasterCard method',
+    'jcb'              =>      'JCB method',
+    'cod'              =>      'Cash on Delivery - COD.',
+    'nganluong'        =>      'Provided by NganLuong.',
+    'payment'          =>      'Payment Method',
+    'support'          =>      'Customer Support',
+  ];
---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+  <!-- To the right -->
+  <div class="pull-right hidden-xs">
+    <b>AdminLTE</b> 2.4.18
+  </div>
+  <!-- Default to the left -->
+  <strong>Copyright &copy; 2020 <a href="{{ route('home_page') }}">CTA Store</a>.</strong> All rights reserved.
+</footer>
